2f97c46d9cb5af8535fb01f034ecebfdab8259eb
pieter-sys <pieter.melkenbeke@gmail.com>
2025-09-22 00:36:28 +0200

    started work on player triangle

--- main.py
+++ main.py
@@ -6,15 +6,25 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
+        # handle quit
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # draw
         screen.fill((0, 0, 0))
+
+        # present
         pygame.display.flip()
 
-
+        # set 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
