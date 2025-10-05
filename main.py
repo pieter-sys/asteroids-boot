8927b250ac05c9955be761ff5ce8a27981dcfeab
pieter-sys <pieter.melkenbeke@gmail.com>
2025-10-05 04:09:27 +0200

    added moving asteroids, began bullet work

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
-# from tkinter.constants import S
 import pygame
-from constants import *
+import constants
+import sys
 from circleshape import CircleShape
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
     pygame.init()
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
     dt = 0
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     CircleShape.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    Shot.containers = (shots, updatable, drawable)
 
-    whitestar = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    whitestar = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+    field = AsteroidField()
+
+    dt = clock.tick(60) / 1000
 
     while True:
         # handle quit
@@ -31,6 +41,12 @@ def main():
 
         # update
         updatable.update(dt)
+
+        # collision detection
+        for asteroid in asteroids:
+            if asteroid.collision(whitestar):
+                print("Game Over!")
+                sys.exit()
 
         # draw
         screen.fill((0, 0, 0))
--- player.py
+++ player.py
@@ -1,6 +1,12 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import (
+    PLAYER_RADIUS,
+    PLAYER_TURN_SPEED,
+    PLAYER_SPEED,
+    SHOT_RADIUS,
+    PLAYER_SHOOT_SPEED,
+)
 
 
 class Player(CircleShape):
@@ -33,7 +39,25 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        pass
+
+
+class Shot(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
+
+
+def draw(self, screen):
+    pygame.draw.circle(screen, "white", self.position, SHOT_RADIUS, width=2)
+
+
+def update(self, dt):
+    self.position += self.velocity * dt
